b9fcb01efde88fd14fba7d3c1f886b73d6f92f98
CardosoCleiton <cleiton_kssa@hotmail.com>
2020-11-20 14:33:17 -0300

    criado evento btn like deslike e show text post

--- src/components/Comentarios.js
+++ src/components/Comentarios.js
@@ -8,6 +8,46 @@ import '../styles/elements/comentarios.css'
 
 export default class Comentarios extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            like: 0,
+            liketruefalse: true,
+            visible:false
+        }
+
+        this.likes= this.likes.bind(this);
+        this.visibleplus = this.visibleplus.bind(this);
+    }
+
+    likes(e){
+        e.preventDefault();
+        if(this.state.liketruefalse === true){
+            this.setState({
+                like: this.state.like + 1,
+                liketruefalse: false
+            })
+        }else{
+            this.setState({
+                like: this.state.like - 1,
+                liketruefalse: true
+            })
+        }
+    }
+
+    visibleplus(e){
+        e.preventDefault();
+        if(this.visible){
+            this.setState({
+                visible: !this.state.visible
+            })
+        }else{
+            this.setState({
+                visible: !this.state.visible
+            })
+        }
+    }
+
     render(){
         return(
 
@@ -17,20 +57,20 @@ export default class Comentarios extends React.Component {
                     <p className ="txtAssunto">{this.props.assunto}</p>
                     <p className ="nomeUser">Carlos Henrique Santos</p>
                     </div>
-                    <div className= "divComentario">
+                    <div className={this.state.visible ? "divComentarioVisible" : "divComentario" }>
                     <p className= "txtComentario">{this.props.conteudo}</p>
                     </div>
                     <div className = "divImgs">
                         
                         <div className ="div_Img_tres_p">
-                        <img className ="img_tres_p" src={img_1}></img>
+                        <img onClick={this.visibleplus} className ="img_tres_p" src={img_1}></img>
                         </div>
 
                         <div className="coracao">
-                        <img src={heart}></img>
+                        <img onClick={this.likes} src={heart}></img>
                         </div>
                         
-                        <p className="like">1 like</p>
+                        <p className="like">{this.state.like} like</p>
                         <p className="resposta">1 resposta</p>
 
                     </div>
